79fb7a1e56635883a0a3f27aa8a92a2fd7b7a612
georgi9714 <illidanaaq@gmail.com>
2018-04-16 15:09:13 +0200

    commit2

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace dnpsecurity
             throw new NotImplementedException();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the GHTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseStaticFiles();
@@ -55,7 +55,7 @@ namespace dnpsecurity
             }
 
             app.UseStaticFiles();
-
+//comment
             app.UseMvc();
         }
     }
